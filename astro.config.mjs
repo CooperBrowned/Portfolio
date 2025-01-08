a11d01274bc051131c47a6c863b1f9ceff92a924
Cooper Brown <cbrown54@terpmail.umd.edu>
2025-01-08 12:22:03 -0500

    fixed routing and added final page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,11 @@ export default defineConfig({
     redirects: {
         '/projects/lightweight-autonomous-suas': '/Autonomous Drone Software',
         '/projects/exoskeletal-composite-reinforcement': '/Invincible Aerostructures',
-        'projects/aerodynamic-guided-parachute': '/Aerodynamic Guided Parachute',
-        'projects/rotary-guided-parachute': '/Rotor Guided Parachute',
-        'projects/l1-rocket': '/L1 High Power Rocket',
-        'projects/enes102-truss': '/Highest Performing Truss'
-
+        '/projects/aerodynamic-guided-parachute': '/Aerodynamic Guided Parachute',
+        '/projects/rotary-guided-parachute': '/Rotor Guided Parachute',
+        '/projects/l1-rocket': '/L1 High Power Rocket',
+        '/projects/enes102-truss': '/Highest Performing Truss',
+        '/projects/enes100-otv': '/OTV Robot Finalist',
+        '/projects/generative-ai-interface': '/Local AI Chatbot'
       }
 });
